13e36db0b0902b74604e1d74446203c70193c478
iwant <53087594+1want@users.noreply.github.com>
2022-07-26 23:32:15 +0800

    优化audio时间监听粒度
    使用定时器代替audio的回调事件

--- src/js/index.js
+++ src/js/index.js
@@ -31,86 +31,70 @@ function createLyric(lyric, audio) {
   let timer = null
   audio.addEventListener('play', () => {
     timer = setInterval(() => {
-      let target = audio.currentTime
-      console.log(target)
+      let target = Math.floor(audio.currentTime * 1000)
+      const s = document.getElementsByTagName('span')
+      const li = document.getElementsByTagName('li')
+
+      function startPlay() {
+        lyrics.find((item, index) => {
+          let end = item.time.indexOf(',')
+          let t = Math.floor(item.time.slice(1, end))
+          let spanI = 0
+
+          let t1 = target - t > 50 && target - t < 500 //完
+          let t2 = t - target > 50 && t - target < 500 //早
+          let res1 = target - t
+          let res2 = t - target
+          function addStyle() {
+            if (t1 || t2) {
+              li[index].childNodes[spanI].className = 'currentSpan'
+              let time = null
+              if (spanI === item.time2.length - 1) {
+                if (item.time2[spanI] > 1) {
+                  time = 1
+                } else {
+                  time = item.time2[spanI] / 1000
+                }
+              } else {
+                time = item.time2[spanI] / 1000
+              }
+
+              if (t1) {
+                li[index].childNodes[spanI].style.animationDuration =
+                  time - res1 / 1000 + 's'
+              } else {
+                li[index].childNodes[spanI].style.animationDuration =
+                  time + res2 / 1000 + 's'
+              }
+              spanI++
+            }
+          }
+
+          async function addSleep() {
+            addStyle()
+            if (spanI !== item.time2.length - 1) {
+              if (t1) {
+                await sleep(item.time2[spanI] - res1 / 100)
+              } else {
+                await sleep(item.time2[spanI] + res2 / 100)
+              }
+            }
+
+            if (spanI < item.time2.length) {
+              addSleep()
+            } else {
+              startPlay()
+            }
+          }
+          addSleep()
+        })
+      }
+      startPlay()
     }, 50)
   })
   audio.addEventListener('pause', () => {
     clearInterval(timer)
   })
-
-  // audio.addEventListener('timeupdate', function (e) {
-  //   console.log(e.target.currentTime)
-  //   playStatus = true
-
-  //   let target = Math.floor(e.target.currentTime * 1000000)
-  //   const s = document.getElementsByTagName('span')
-  //   const li = document.getElementsByTagName('li')
-
-  //   function startPlay() {
-  //     start = true
-  //     lyrics.find((item, index) => {
-  //       let end = item.time.indexOf(',')
-  //       let t = Math.floor(item.time.slice(1, end) * 1000)
-  //       let spanI = 0
-
-  //       let t1 = target - t > 3000 && target - t < 200000 //完
-  //       let t2 = t - target > 3000 && t - target < 200000 //早
-  //       let res1 = target - t
-  //       let res2 = t - target
-  //       function addStyle() {
-  //         if (playStatus) {
-  //           if (t1 || t2) {
-  //             li[index].childNodes[spanI].className = 'currentSpan'
-  //             let time = null
-  //             if (spanI === item.time2.length - 1) {
-  //               if (item.time2[spanI] / 1000 > 1) {
-  //                 time = 1
-  //               } else {
-  //                 time = item.time2[spanI] / 1000
-  //               }
-  //             } else {
-  //               time = item.time2[spanI] / 1000
-  //             }
-
-  //             if (t1) {
-  //               li[index].childNodes[spanI].style.animationDuration =
-  //                 time - res1 / 1000000 + 's'
-  //             } else {
-  //               li[index].childNodes[spanI].style.animationDuration =
-  //                 time + res2 / 1000000 + 's'
-  //             }
-  //             spanI++
-  //           } else {
-  //             start = false
-  //           }
-  //         }
-  //       }
-
-  //       async function addSleep() {
-  //         addStyle()
-  //         if (spanI !== item.time2.length - 1) {
-  //           if (t1) {
-  //             await sleep(item.time2[spanI] - res1 / 100000)
-  //           } else {
-  //             await sleep(item.time2[spanI] + res2 / 100000)
-  //           }
-  //         }
-
-  //         if (spanI < item.time2.length) {
-  //           addSleep()
-  //         } else {
-  //           start = false
-  //           startPlay()
-  //         }
-  //       }
-  //       addSleep()
-  //     })
-  //   }
-  //   if (!start) {
-  //     startPlay()
-  //   }
-  // })
 
   const play = () => {
     playStatus = true
