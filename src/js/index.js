4f22e842be4d852a53b42abbb553574908f787a2
iwant <53087594+1want@users.noreply.github.com>
2022-07-28 09:24:50 +0800

    ts3

--- src/js/index.js
+++ src/js/index.js
@@ -19,28 +19,28 @@ function drawLyric(lyric) {
 
 function createLyric(lyric, audio) {
   const lyrics = parseLyric(lyric)
-  // console.log(lyrics)
+  console.log(lyrics)
   var playStatus = false
 
   drawLyric(lyrics)
 
   // 粒度间隔最大300毫秒
-
   let timer = null
-  audio.addEventListener('play', () => {
+
+  function fn() {
     const li = document.getElementsByTagName('li')
 
     timer = setInterval(() => {
-      let target = Math.floor(audio.currentTime * 1000)
+      let target = Math.floor(audio.currentTime * 10000)
 
       // 每次查找到新的index，通过li[index]确定需要渲染哪一行，li是获取的所有li
       lyrics.find((item, index) => {
         let end = item.time.indexOf(',')
-        let t = Math.floor(item.time.slice(1, end))
+        let t = Math.floor(item.time.slice(1, end) * 10)
         let spanI = 0
 
-        let t1 = target - t > 30 && target - t < 200 //完
-        let t2 = t - target > 30 && t - target < 200 //早
+        let t1 = target - t > 500 && target - t < 1000 //完
+        let t2 = t - target > 500 && t - target < 1000 //早
         let res1 = target - t
         let res2 = t - target
         function addStyle() {
@@ -51,19 +51,23 @@ function createLyric(lyric, audio) {
               if (item.time2[spanI] > 1) {
                 time = 1
               } else {
-                time = item.time2[spanI] / 1000
+                time = item.time2[spanI] / 10000
               }
             } else {
-              time = item.time2[spanI] / 1000
+              time = item.time2[spanI] / 1000 - 90 / 1000
             }
 
             if (t1) {
               li[index].childNodes[spanI].style.animationDuration =
-                time - res1 / 1000 + 's'
-            } else {
-              li[index].childNodes[spanI].style.animationDuration =
-                time + res2 / 1000 + 's'
+                time - res1 / 10000 + 's'
             }
+            if (t2) {
+              li[index].childNodes[spanI].style.animationDuration =
+                time + res2 / 10000 + 's'
+            }
+
+            console.log(li[index].childNodes[spanI].style.animationDuration)
+
             spanI++
           }
         }
@@ -71,23 +75,32 @@ function createLyric(lyric, audio) {
         async function addSleep() {
           if (t1 || t2) {
             addStyle()
+            clearInterval(timer)
             if (spanI !== item.time2.length - 1) {
               if (t1) {
-                await sleep(item.time2[spanI] - res1 / 1000)
+                console.log(item.time2[spanI] - res1 / 10000)
+                await sleep(item.time2[spanI] - res1 / 10000)
               }
               if (t2) {
-                await sleep(item.time2[spanI] + res2 / 1000)
+                console.log(item.time2[spanI] - res1 / 10000, spanI)
+
+                await sleep(item.time2[spanI] + res2 / 10000)
               }
             }
 
             if (spanI < item.time2.length) {
               addSleep()
+            } else {
+              // fn()
             }
           }
         }
         addSleep()
       })
-    }, 30)
+    }, 20)
+  }
+  audio.addEventListener('play', () => {
+    fn()
   })
   audio.addEventListener('pause', () => {
     clearInterval(timer)
