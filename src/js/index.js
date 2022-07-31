e60b680962e94eb93b1d71fb6a8ea59a9dfd20a0
iwant <53087594+1want@users.noreply.github.com>
2022-07-31 17:47:03 +0800

    添加播放暂停

--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,72 @@
-//每个字渲染完之后需要sleep当前字的时间，然后判断递归渲染下一个字还是下一行
-
+import { sleep, drawLyric } from '../utils/index.js'
 import parseLyric from './parseLyric.js'
-
-function drawLyric(lyric) {
-  const body = document.getElementsByTagName('body')[0]
-  const Ul = document.createElement('ul')
-  for (var i of lyric) {
-    const li = document.createElement('li')
-    for (var j of i.lyric) {
-      const span = document.createElement('span')
-
-      span.innerText = j
-      li.append(span)
-    }
-    Ul.append(li)
-  }
-
-  body.append(Ul)
-}
 
 function createLyric(lyric, audio) {
   const lyrics = parseLyric(lyric)
+  let spanI = 0
+  let index = 0
+  let playState = false
+  let playTime = 0
+  let item = null
+  let setTimer = null
+  const li = document.getElementsByTagName('li')
+
   drawLyric(lyrics)
 
-  const li = document.getElementsByTagName('li')
-  audio.addEventListener('play', () => {
-    let spanI = 0
-    let index = 0
-    let item = null
-    function addStyle() {
-      item = lyrics[index]
-      li[index].childNodes[spanI].className = 'currentSpan'
-      let time = null
-      time = item.time2[spanI] / 1000
-      li[index].childNodes[spanI].style.animationDuration = time + 's'
-    }
-    async function addSleep() {
-      addStyle()
-      await sleep(item.time2[spanI])
-      spanI++
-      if (spanI < item.time2.length) {
-        addSleep()
-      } else {
-        index++
-        spanI = 0
-        addSleep()
-      }
-    }
-
-    setTimeout(() => {
+  function addStyle() {
+    item = lyrics[index]
+    li[index].childNodes[spanI].className = 'currentSpan'
+    let time = null
+    time = item.time2[spanI] / 1000
+    li[index].childNodes[spanI].style.animationDuration = time + 's'
+  }
+  async function addSleep() {
+    if (!playState) return
+    addStyle()
+    await sleep(item.time2[spanI])
+    spanI++
+    if (spanI < item.time2.length) {
       addSleep()
-    }, 16650)
-  })
-}
+    } else {
+      index++
+      spanI = 0
+      addSleep()
+    }
+  }
 
-const sleep = delay => {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay)
-  })
+  const play = () => {
+    //  暂停音乐时,spanI可能已经到了下一个字体，导致即将要渲染的字无法正确渲染
+    // 所以需要给当前字和上一个字都加上running
+    audio.play()
+    playState = true
+
+    if (spanI >= 0) {
+      li[index].childNodes[spanI - 1 < 0 && 0].style.animationPlayState =
+        'running'
+      li[index].childNodes[spanI].style.animationPlayState = 'running'
+    }
+
+    if (16650 - playTime * 1000 > 0) {
+      setTimer = setTimeout(() => {
+        addSleep()
+      }, 16650 - playTime * 1000)
+    } else {
+      addSleep()
+    }
+  }
+  const pause = () => {
+    // 频繁点击暂停时，会导致字体渲染偏移量增加
+    audio.pause()
+    clearTimeout(setTimer)
+    playTime = audio.currentTime
+    playState = false
+    li[index].childNodes[spanI].style.animationPlayState = 'paused'
+  }
+
+  return {
+    play,
+    pause
+  }
 }
 
 export default createLyric
